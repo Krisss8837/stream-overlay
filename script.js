875c0746ab43c18ee47b9b6965facf7ad40e142a
Krisss8837 <sobalakrzyszof@gmail.com>
2026-07-22 12:40:05 +0200

    oweral do tiktoka

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let followers = 0;
+// Główny plik skryptu dla nakładki StreamToEarn
 
-setInterval(()=>{
-    followers++;
-    document.getElementById("counter").innerHTML =
-    "Followers: " + followers;
-},3000);
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Nakładka StreamToEarn została pomyślnie załadowana.");
+
+    // Opcjonalnie: Możesz tutaj dodać funkcję pomocniczą, 
+    // jeśli potrzebujesz obsługiwać niestandardowe zdarzenia w JavaScript.
+});
