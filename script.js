0ba45e53ed53bbaf34bfd6ca8384710f470a2388
Krisss8837 <sobalakrzyszof@gmail.com>
2026-07-22 13:41:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Główny plik skryptu dla nakładki StreamToEarn
 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Nakładka StreamToEarn została pomyślnie załadowana.");
-
-    // Opcjonalnie: Możesz tutaj dodać funkcję pomocniczą, 
-    // jeśli potrzebujesz obsługiwać niestandardowe zdarzenia w JavaScript.
-});
